f0224cfe1706f0480a0e42cdab83f472e1dcc731
kawaharashotaro <kkawaharasshoutarou@gmail.com>
2020-05-27 22:05:07 +0900

    edit comment

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
-// q1 mとn足した値を返す関数Sumを完成せよ
-func Add (m, n int) int {
+// q1 mとn足した値を返す関数Addを完成せよ
+func Add(m, n int) int {
 	return 0
 }
 
 // q2 1からnまでの足した値を返す関数Sumを完成せよ
 // ただし n > 0とする
-func Sum (n int) int {
+func Sum(n int) int {
 	return 0
 }
-
-
